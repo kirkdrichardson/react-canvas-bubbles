2927cbf8b614b22b0fda1ac940f3c01468dfffaa
Kirk <kirk.douglas.richardson@gmail.com>
2018-11-10 16:04:29 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer>
+        <Header>Bubbles!</Header>
+      </AppContainer>
     );
   }
 }
+
+
+const headerHeight = 100;
+
+const AppContainer = styled.div`
+  padding: 20px;
+  marginTop: ${headerHeight}px;
+  color: white;
+  font-size: 30px;
+  position: relative;
+`;
+
+const Header = styled.div`
+  background-color: green;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: ${headerHeight}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 export default App;
